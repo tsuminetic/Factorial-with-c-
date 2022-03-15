88021f14a86ad821a2855f00e437792086c60dda
Acer <Acer@TSUMI>
2022-03-15 22:15:02 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿static int Fact(int x)
+{
+    for(int i = x-1; i >0; i--)
+    {
+        x = x * i;
+    }
+    return x;
+}
+
+
+Console.WriteLine("Enter your number:");
+int x=Convert.ToInt32(Console.ReadLine());
+for(int i = x; i >=1; i--)
+{
+    Console.Write(i);
+    if(i == 1)
+    {
+        Console.Write("=");
+    }
+    else
+    {
+        Console.Write("x");
+    }
+}
+Console.WriteLine(Fact(x));
